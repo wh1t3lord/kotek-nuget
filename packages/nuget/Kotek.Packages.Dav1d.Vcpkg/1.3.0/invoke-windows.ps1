706812b405b73e4367fda6cfeefdc49dd3bbd408
wh1t3lord <git.matvey@yandex.ru>
2024-09-19 19:43:29 +0300

    ADD: gitignore

--- packages/nuget/Kotek.Packages.Dav1d.Vcpkg/1.3.0/invoke-windows.ps1
+++ packages/nuget/Kotek.Packages.Dav1d.Vcpkg/1.3.0/invoke-windows.ps1
@@ -3,6 +3,8 @@ param (
     [string] $CompilerName
 )
 
+$Output = "out"
+
 $isWin11 = (Get-WmiObject Win32_OperatingSystem).Caption -Match "Windows 11"
 $isWin10 = (Get-WmiObject Win32_OperatingSystem).Caption -Match "Windows 10"
 $isWin7 = (Get-WmiObject Win32_OperatingSystem).Caption -Match "Windows 7"
@@ -10,6 +12,7 @@ $isWin7 = (Get-WmiObject Win32_OperatingSystem).Caption -Match "Windows 7"
 $Arch = (Get-Process -Id $PID).StartInfo.EnvironmentVariables["PROCESSOR_ARCHITECTURE"];
 
 $PlatformBit = ""
+$PlatformName = ""
 
 if ($Arch -eq 'x86') {
     $PlatformBit = "x86"
@@ -21,8 +24,9 @@ elseif ($Arch -eq 'amd64') {
 # TODO: add other windows operating system versions
 if ($isWin11) {
     Write-Output "executing for Windows 11 with compiler $CompilerName | platform $Arch ($PlatformBit)"
+    $PlatformName = "win11"
 
-    
+    nuget pack $PSScriptRoot\metapackage-$PlatformName-$CompilerName-$PlatformBit.nuspec -OutputDirectory $Output
 
 }
 elseif ($isWin10) {
