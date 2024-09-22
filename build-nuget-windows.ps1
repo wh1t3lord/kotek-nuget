a78e37315aa1deb6e0287414a87b28b811cd3605
wh1t3lord <git.matvey@yandex.ru>
2024-09-22 14:58:14 +0300

    ADD: win11 - packages

--- build-nuget-windows.ps1
+++ build-nuget-windows.ps1
@@ -20,6 +20,7 @@ Write-Output "Platform is $OsName"
 $VersionFolderDav1dName = ""
 $VersionFolderDirectXMathName = ""
 $VersionFolderCgltfName = ""
+$VersionFolderEigen3Name = ""
 if ($Latest -eq "1") {
     $VersionFolderDav1dName = (Get-ChildItem -Directory -Path .\packages\nuget\Kotek.Packages.Dav1d.Vcpkg\ -Force -Recurse | Select-Object -Last 1)
     $VersionFolderDirectXMathName = (Get-ChildItem -Directory -Path .\packages\nuget\Kotek.Packages.DirectXMath.Vcpkg\ -Force -Recurse | Select-Object -Last 1)
@@ -29,8 +30,10 @@ else {
     $VersionFolderDav1dName = (Get-ChildItem -Directory -Path .\packages\nuget\Kotek.Packages.Dav1d.Vcpkg\ -Force -Recurse | Select-Object -First 1)
     $VersionFolderDirectXMathName = (Get-ChildItem -Directory -Path .\packages\nuget\Kotek.Packages.DirectXMath.Vcpkg\ -Force -Recurse | Select-Object -First 1)
     $VersionFolderCgltfName = (Get-ChildItem -Directory -Path .\packages\nuget\Kotek.Packages.Cgltf.Vcpkg\ -Force -Recurse | Select-Object -First 1)
+    $VersionFolderEigen3Name = (Get-ChildItem -Directory -Path .\packages\nuget\Kotek.Packages.Eigen3.Vcpkg\ -Force -Recurse | Select-Object -First 1)
 }
 
 . .\build-windows.ps1 -Type nuget -Name Kotek.Packages.Dav1d.Vcpkg -Version $VersionFolderDav1dName -CompilerName $CompilerName
 . .\build-windows.ps1 -Type nuget -Name Kotek.Packages.DirectXMath.Vcpkg -Version $VersionFolderDirectXMathName -CompilerName $CompilerName
 . .\build-windows.ps1 -Type nuget -Name Kotek.Packages.Cgltf.Vcpkg -Version $VersionFolderCgltfName -CompilerName $CompilerName
+. .\build-windows.ps1 -Type nuget -Name Kotek.Packages.Eigen3.Vcpkg -Version $VersionFolderEigen3Name -CompilerName $CompilerName
--- packages/nuget/Kotek.Packages.Cgltf.Vcpkg/1.13/runtimes.win11-msvc-x64.cmake
+++ packages/nuget/Kotek.Packages.Cgltf.Vcpkg/1.13/runtimes.win11-msvc-x64.cmake
@@ -5,8 +5,8 @@ elseif($<CONFIG> STREQUAL "Release" OR $<CONFIG> STREQUAL "MinSizeRel" OR $<CONF
 endif()
 
 set(CGLTF_ROOT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../..")
-set(CGLTF_RUNTIME_ID "win10-msvc-x64")
-set(CGLTF_RUNTIME_PATH "${CGLTF_ROOT_PATH}/runtimes/win10/msvc/x64/")
-set(CGLTF_INCLUDE_PATH "${CGLTF_ROOT_PATH}/runtimes/win10/msvc/x64/include/")
+set(CGLTF_RUNTIME_ID "win11-msvc-x64")
+set(CGLTF_RUNTIME_PATH "${CGLTF_ROOT_PATH}/runtimes/win11/msvc/x64/")
+set(CGLTF_INCLUDE_PATH "${CGLTF_ROOT_PATH}/runtimes/win11/msvc/x64/include/")
 
 target_include_directories(${PROJECT_NAME} PRIVATE ${CGLTF_INCLUDE_PATH})
